4637b953352b9c617fe1ba8f94aa14c2f6a3236c
shiki <luolinman_siki@163.com>
2025-02-28 11:18:52 +0800

    提交 Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeScript } from "@/components/layout/ThemeToggle";
+import GoogleAnalytics from '@/components/GoogleAnalytics'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       </head>
       <body className={`${inter.className} bg-white dark:bg-gray-900`}>
         {children}
+        <GoogleAnalytics GA_MEASUREMENT_ID="G-SSG1FNP0LX" />
       </body>
     </html>
   );
